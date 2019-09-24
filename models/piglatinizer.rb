2dc335ab9ba0ea499f5ba9468eaae04df995e5c4
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-09-24 21:34:41 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,15 +1,22 @@
 class Piglatinizer
-  attr_reader :text
+  attr_reader :string
 
-  def initialize(text)
-    @text = text
+  def initialize(string)
+    @string = string
   end
 
- def reverse_string(text)
-  split_string = text("")
-  reversed = []
-  text.size.times { reversed << split_string.pop }
-  reversed.join
+  def consonant(vowel)
+    !vowel.match(/[aAeEoOuUiI]/)
+    string << vowel
+  end
+
+ def reverse_string(string)
+   string_reverse = string.split("")
+   reversed = []
+   string.size.times { reversed << string_reverse.pop }
+   reversed.join
  end
+
+
 
 end
