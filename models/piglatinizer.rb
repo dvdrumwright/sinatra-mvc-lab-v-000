1b5477d8384de989f89f52692932e97c9bac3015
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-09-24 22:09:46 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,17 +1,18 @@
 class Piglatinizer
 
-
-  def PigLatinizer(word)
-    string_reverse = word.split("")
-    reversed = []
-    word.size.times { reversed << string_reverse.pop }
-    reversed.join
+  def PigLatinizer (sent)
+      vowels = %w{a e i o u}
+      sent.gsub(/(\A|\s)\w+/) do |str|
+              str.strip!
+          while not vowels.include? str[0] or (str[0] == 'u' and str[-1] == 'q')
+              str += str[0]
+              str = str[1..-1]
+          end
+          str  = ' ' + str + 'ay'
+      end.strip
   end
 
-  def consonant(vowel)
-    !vowel.match(/[aAeEoOuUiI]/)
-    string << vowel
-  end
+  
 
 
 
